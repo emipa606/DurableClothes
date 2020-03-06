b3bb79d7f98fdbfb68c510b1a9d78698a15481eb
emipa606 <emipa606@users.noreply.github.com>
2020-03-06 22:38:05 +0100

    Add files via upload

---
+++ Source/DurableClothes_Mod.cs
@@ -0,0 +1,54 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedParameter.Global
+// ReSharper disable InconsistentNaming
+
+using HarmonyLib;
+using HugsLib;
+using HugsLib.Settings;
+using RimWorld;
+using System.Reflection;
+using Verse;
+
+namespace DurableClothes
+{
+    public class DurableClothes_Settings : ModBase
+    {
+        public override string ModIdentifier
+        {
+            get { return "DurableClothes"; }
+        }
+
+        public static SettingHandle<bool> toggleFullRepair;
+
+        public override void DefsLoaded()
+        {
+            toggleFullRepair = Settings.GetHandle("toggleFullRepair",
+                // TODO translation support
+                "Full Repair",
+                "When enabled, fully repairs all clothing instead of simply stopping degradation.",
+                true);
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn_ApparelTracker))]
+    [HarmonyPatch("TakeWearoutDamageForDay")]
+    internal class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            Harmony harmony = new Harmony("rimworld.mlie.durableclothes");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPrefix]
+        public static bool PatchMethod_Prefix(ref Thing ap)
+        {
+            if ((bool)DurableClothes_Settings.toggleFullRepair) // Only do full repair if the setting is enabled
+            {
+                ap.HitPoints = ap.MaxHitPoints;
+            }
+            return false; // don't run the original logic to degrade apparel either way
+        }
+    
+    }
+}
