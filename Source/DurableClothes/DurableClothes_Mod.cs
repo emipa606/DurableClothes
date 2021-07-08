1ec19dc715b8c61d43c2467bc036b598fea5aa4e
emipa606 <emil.offermo@litet.net>
2021-07-08 22:53:15 +0200

    Mod updated for 1.3 and passed autotests

--- Source/DurableClothes/DurableClothes_Mod.cs
+++ Source/DurableClothes/DurableClothes_Mod.cs
@@ -2,21 +2,20 @@
 // ReSharper disable UnusedParameter.Global
 // ReSharper disable InconsistentNaming
 
+using System.Reflection;
 using HarmonyLib;
 using HugsLib;
 using HugsLib.Settings;
 using RimWorld;
-using System.Reflection;
 using Verse;
 
 namespace DurableClothes
 {
     public class DurableClothes_Settings : ModBase
     {
-        public override string ModIdentifier => "DurableClothes";
-
         public static SettingHandle<bool> toggleFullRepair;
         public static SettingHandle<int> wearPercent;
+        public override string ModIdentifier => "DurableClothes";
 
         public override void DefsLoaded()
         {
@@ -30,7 +29,6 @@ namespace DurableClothes
                 "Wear damage %",
                 "Instead of no deterioration, you can change it to a percent of the original here.",
                 0);
-
         }
     }
 
@@ -47,29 +45,35 @@ namespace DurableClothes
         [HarmonyPrefix]
         public static bool PatchMethod_Prefix(ref Thing ap, Pawn_ApparelTracker __instance)
         {
-            if ((bool)DurableClothes_Settings.toggleFullRepair) // Only do full repair if the setting is enabled
+            if ((bool) DurableClothes_Settings.toggleFullRepair) // Only do full repair if the setting is enabled
             {
                 ap.HitPoints = ap.MaxHitPoints;
                 //Log.Message("Repair to full");
                 return false;
             }
+
             if (DurableClothes_Settings.wearPercent == 0)
             {
                 //Log.Message("No deterioraton");
                 return false;
             }
+
             var num = GenMath.RoundRandom(ap.def.apparel.wearPerDay * DurableClothes_Settings.wearPercent / 100);
             //Log.Message("Take damage "+ num);
             if (num > 0)
             {
-                ap.TakeDamage(new DamageInfo(DamageDefOf.Deterioration, (float)num, 0f, -1f, null, null, null, DamageInfo.SourceCategory.ThingOrUnknown, null));
+                ap.TakeDamage(new DamageInfo(DamageDefOf.Deterioration, num));
             }
+
             if (ap.Destroyed && PawnUtility.ShouldSendNotificationAbout(__instance.pawn) && !__instance.pawn.Dead)
             {
-                Messages.Message("MessageWornApparelDeterioratedAway".Translate(GenLabel.ThingLabel(ap.def, ap.Stuff, 1), __instance.pawn).CapitalizeFirst(), __instance.pawn, MessageTypeDefOf.NegativeEvent, true);
+                Messages.Message(
+                    "MessageWornApparelDeterioratedAway"
+                        .Translate(GenLabel.ThingLabel(ap.def, ap.Stuff), __instance.pawn).CapitalizeFirst(),
+                    __instance.pawn, MessageTypeDefOf.NegativeEvent);
             }
+
             return false; // don't run the original logic to degrade apparel either way
         }
-
     }
 }
